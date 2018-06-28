5cded856818e51c5a361dd445ed9455081374efe
AssafBarNaatan <assafbarnatan@gmail.com>
2018-06-28 15:38:36 -0400

    Just some edits

--- rundmcmc/chain_to_benchmark.py
+++ rundmcmc/chain_to_benchmark.py
@@ -7,44 +7,67 @@ Created on Thu Jun 28 10:57:56 2018
 
 ###All the tools to take the output of chain and make it useful:
 
-
-from rundmcmc.grid import Grid
+from grid import Grid
 
 from rundmcmc.chain import MarkovChain
 from rundmcmc.proposals import propose_random_flip
 from rundmcmc.validity import Validator, contiguous, no_vanishing_districts
 from rundmcmc.accept import always_accept
 import numpy as np
+#import rundmcmc.benchmark_distances
+import metagraph_distances as md
 
-def read_chain(graph, iterations):
-  is_valid = Validator([contiguous,no_vanishing_districts])
-  chain = MarkovChain(propose_random_flip, is_valid, always_accept, graph, total_steps = iterations)
-  partitions = []
-  
-  
-  for step in chain: 
-    #print('parent = ')
-    #print(step.parent.assignment)
-    #print('current assignment')
-    #print(step.assignment)
-    #if step.flips:
-      #if not (list(step.flips.keys())[0][0] == list(step.flips.keys())[0][1]):
-  #  print(step)
-    partitions.append(step.assignment)
-    #print('Keys')
-    #print(step.flips)
-    #print(list(step.flips.keys())[0])
-    #print(list(step.flips.keys())[0][0] == list(step.flips.keys())[0][1])
+def dict_invert(dictionary):
+    dict = {val: [key for key in dictionary.keys() if dictionary[key] == val] for val in dictionary.values()}
+    return dict
 
-  #print(partitions)  
-  newlist = [dict(s) for s in set(frozenset(d.items()) for d in partitions)]
-  return partitions
-  print(newlist)
-  #distance_matrix = bmt.build_distance_matrix(newlist)
-  return distance_matrix
+def dictionary_to_sets(dictionary):
+    #print(dictionary[1])
+    return set([frozenset(list) for list in dict_invert(dictionary).values()])
+
+def dictionary_list_to_sets_list(dict_list):
+    return [dictionary_to_sets(x) for x in dict_list]
+
+def chain_to_partitions(graph, iterations, skip_standing = False):
+    is_valid = Validator([contiguous,no_vanishing_districts])
+    chain = MarkovChain(propose_random_flip, is_valid, always_accept, graph, total_steps = iterations)
+    partitions = []
+  
+    for step in chain:
+        if step.flips:
+            if skip_standing:
+                if not (list(step.flips.keys())[0][0] == list(step.flips.keys())[0][1]):
+                    partitions.append(step.assignment)
+            else:
+                partitions.append(step.assignment)
+    return partitions
+
+def test_distance(plan1, plan2):
+    return 1
+
+def build_distance_matrix(partitions, function):
+    n = len(partitions)
+    M = np.zeros([n,n])
+    for i in range(n):
+        for j in range(n):
+            M[i][j] = function(partitions[i], partitions[j])
+    return M
+
+def partitions_to_distance(partition_list, distance_function):
+    #Partition_list is a list of set partitions
+    if type(partition_list[0]) == set:
+        plan = partition_list
+    if type(partition_list[0]) == dict:
+        plan = dictionary_list_to_sets_list(partition_list)
+
+    return build_distance_matrix(plan,distance_function)
 
 def main():
-  grid = Grid((3,3))
-  print(grid)
-  distance_matrix = read_chain(grid, 10)
-  print(distance_matrix)
+    grid = Grid((10,10))
+    print(grid)
+    partitions = chain_to_partitions(grid, 10)
+    D = partitions_to_distance(partitions, md.stupid_hamming_distance)
+    print(D)
+
+if __name__ == "__main__":
+    main()
--- rundmcmc/metagraph_distances.py
+++ rundmcmc/metagraph_distances.py
@@ -4,19 +4,7 @@ Created on Wed Jun 27 10:41:37 2018
 
 @author: Assaf, Lorenzo, Anthony
 """
-from itertools import product
 import numpy as np
-import numpy as np
-
-from matplotlib import pyplot as plt
-from matplotlib.collections import LineCollection
-
-#from sklearn import manifold
-#from sklearn.metrics import euclidean_distances
-#from sklearn.decomposition import PCA
-
-
-#from scipy.stats import gaussian_kde
 
 def common_refinement(partition1, partition2):
     refinement = {part1.intersection(part2) for part1 in partition1 for part2 in partition2}
@@ -33,43 +21,9 @@ def shared_information_distance(partition1, partition2):
   return(2*partition_entropy(common_refinement(partition1,partition2)) - partition_entropy(partition1) - partition_entropy(partition2))
   
 def stupid_hamming_distance(partition1, partition2):
+    stupid_hamming=0
     for part1 in partition1:
-        stupid_hamming += min([part2.symmetric_difference(part1) for part2 in partition2])
+        stupid_hamming += min([len(part2.symmetric_difference(part1)) for part2 in partition2])
     for part2 in partition2:
-        stupid_hamming += min([part1.symmetric_difference(part2) for part1 in partition1])
-  return(stupid_hamming)
-
-def subgraph_list_to_dictionary(subgraph):
-    m = len(subgraph)
-    node_lists = [g.nodes() for g in subgraph]
-    dictionary = {}
-    for i in range(m):
-        for x in node_lists[i]:
-            dictionary[x] = i
-    return dictionary
-
-def partition_list_to_dictionary_list(partitions):
-    dictionary_list = []
-    for x in partitions:
-        dictionary_list.append(subgraph_list_to_dictionary(x))
-    return dictionary_list
-
-def lp_distance(p1, p2, p):
-    if set(p1.keys()) != set(p2.keys()):
-        return "Keys do not match!"
-    
-    keys = p1.keys()
-    difference_vector = []
-    for k in keys:
-            difference_vector.append( p1[k] - p2[k])
-    return np.linalg.norm(difference_vector, p)
-
-def build_distance_matrix(partitions):
-    #Partition should be stored as dictionaries 
-    n = len(partitions)
-    M = np.zeros([n,n])
-    for i in range(n):
-        for j in range(n):
-            M[i][j] = shared_information_distance(partitions[i], partitions[j])
-            #M[i][j] = stupid_hamming_distance(partitions[i], partitions[j])
-    return M
+        stupid_hamming += min([len(part1.symmetric_difference(part2)) for part1 in partition1])
+    return(stupid_hamming)

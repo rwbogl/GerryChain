53fb50d512afa86b7e491001ca7daf720c1c7465
AssafBarNaatan <assafbarnatan@gmail.com>
2018-06-28 21:12:57 -0400

    added sqent - might not be a positive distance function...

--- rundmcmc/metagraph_distances.py
+++ rundmcmc/metagraph_distances.py
@@ -19,11 +19,24 @@ def partition_entropy(partition):
     #print(number_of_units)
     area_ratios = [len(part)/number_of_units for part in partition]
     #print(area_ratios)
-    return sum([-(prob * np.log(prob)) for prob in area_ratios])
+    #return sum([-(prob * np.log(prob)) for prob in area_ratios])
+    return -np.dot(area_ratios,log(area_ratios))
+
+def partition_sqent(partition):
+    """Returns the entropy of a partition"""
+    number_of_units = len(set().union(*partition))
+    #print(number_of_units)
+    area_ratios = [len(part)/number_of_units for part in partition]
+    #print(area_ratios)
+    #return sum([-(prob * np.log(prob)) for prob in area_ratios])
+    return -np.dot(area_ratios,np.sqrt(area_ratios))
 
 def shared_information_distance(partition1, partition2):
   return(2*partition_entropy(common_refinement(partition1,partition2)) - partition_entropy(partition1) - partition_entropy(partition2))
   
+def shared_sqent_distance(partition1, partition2):
+  return(2*partition_sqent(common_refinement(partition1,partition2)) - partition_sqent(partition1) - partition_sqent(partition2))
+
 def stupid_hamming_distance(partition1, partition2):
     stupid_hamming=0
     for part1 in partition1:
